2538879ef47c15cf6f2cea7d34d5e9b7cf142bf8
Dave <dmarti21@yahoo.com>
2012-07-05 10:11:30 -0400

    initial commit

---
+++ EchoApp/src/org/younghawk/echoapp/EchoMainActivity.java
@@ -0,0 +1,26 @@
+package org.younghawk.echoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class EchoMainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        TextView text = new TextView(this);
+        text.setText("Hello Echo World, from Dave");
+        setContentView(text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_echo_main, menu);
+        return true;
+    }
+
+    
+}
