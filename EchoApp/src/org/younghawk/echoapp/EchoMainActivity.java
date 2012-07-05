a1856be186b93874da169de3bb1110158c0d4bb4
Dave <dmarti21@yahoo.com>
2012-07-05 13:45:49 -0400

    Main view general structure

--- EchoApp/src/org/younghawk/echoapp/EchoMainActivity.java
+++ EchoApp/src/org/younghawk/echoapp/EchoMainActivity.java
@@ -6,21 +6,21 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class EchoMainActivity extends Activity {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        TextView text = new TextView(this);
-        text.setText("Hello Echo World, from Dave");
-        setContentView(text);
+        setContentView(R.layout.activity_echo_main);
+        //TextView text = new TextView(this);
+        //text.setText("Hello Echo World, from Dave");
+        //setContentView(text);
     }
 
+    /**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_echo_main, menu);
         return true;
     }
 
-    
+    **/    
 }
