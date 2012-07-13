979bd029150f5ac788ce4e8eb52d026b8c23f2ba
Dave <dmarti21@yahoo.com>
2012-07-13 14:46:42 -0400

    Changed Activity name to EchoApp

--- org.younghawk.echoapp/src/com/helloandroid/canvastutorial/EchoApp.java
+++ org.younghawk.echoapp/src/com/helloandroid/canvastutorial/EchoApp.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import org.younghawk.echoapp.R;
 
-public class Canvastutorial extends Activity {
+public class EchoApp extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
