a3ff7e9f82f57c2654beb3c3d45e4caa8149918e
Dave <dmarti21@yahoo.com>
2012-07-13 20:26:30 -0400

    updates app name and adds ping button string resource

--- org.younghawk.echoapp/gen/org/younghawk/echoapp/R.java
+++ org.younghawk.echoapp/gen/org/younghawk/echoapp/R.java
@@ -23,6 +23,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int canvas_title=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int ping_button=0x7f040003;
     }
 }
