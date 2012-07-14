b816710e38b041d2777390a01e3029f63c19b696
Dave <dmarti21@yahoo.com>
2012-07-13 20:50:52 -0400

    ping button creates logcat entry

--- org.younghawk.echoapp/src/org/younghawk/echoapp/EchoApp.java
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/EchoApp.java
@@ -2,7 +2,8 @@ package org.younghawk.echoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import org.younghawk.echoapp.R;
+import android.util.Log;
+import android.view.View;
 
 public class EchoApp extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +11,10 @@ public class EchoApp extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void pingButton(View view) {
+    	Log.v("pingButton", "Ping Button Pressed");
     }
     
    
