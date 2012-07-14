d89029ed212a2ed76f76a643bf7b2bfbc05c51d8
Dave <dmarti21@yahoo.com>
2012-07-13 20:38:09 -0400

    cleans up eclispse warnings in xml files

--- org.younghawk.echoapp/gen/org/younghawk/echoapp/R.java
+++ org.younghawk.echoapp/gen/org/younghawk/echoapp/R.java
@@ -22,9 +22,8 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int canvas_title=0x7f040002;
-        public static final int hello=0x7f040000;
-        public static final int ping_button=0x7f040003;
+        public static final int activity_title=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int ping_button=0x7f040002;
     }
 }
