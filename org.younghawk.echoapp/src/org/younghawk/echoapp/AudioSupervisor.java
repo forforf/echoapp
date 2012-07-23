dd186ab50dd57a1bc421a74ecb0e9c6f690a7b9d
Dave <dmarti21@yahoo.com>
2012-07-23 11:38:16 -0400

    rename to AudioSupervisor from SonarSupervisor

---
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/AudioSupervisor.java
@@ -0,0 +1,25 @@
+package org.younghawk.echoapp;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Supervises and handles callbacks for 
+ * audio recording, playing and filtering threads
+ *
+ */
+public class AudioSupervisor {
+	public static AudioSupervisor create() {
+		//kicks of the threads that we will be supervising
+		//and provides them to the constructor
+		return new AudioSupervisor();
+	}
+	
+	private AudioSupervisor() {
+		
+	}
+}
+
+
