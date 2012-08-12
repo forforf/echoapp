064acabdd750b2cebe35fef9d492eb5a76163286
Dave <dmarti21@yahoo.com>
2012-08-11 20:40:17 -0400

    no code change, cleaned up test code in ScrollingBitmap

--- org.younghawk.echoapp/src/org/younghawk/echoapp/drawregion/ScrollingBitmap.java
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/drawregion/ScrollingBitmap.java
@@ -25,10 +25,6 @@ public class ScrollingBitmap {
     private Bitmap mBufferBitmap;
     private Canvas mBufferCanvas;
     
-    //Remove when not needed
-    //private int testing;
-    
-    
     public static ScrollingBitmap create(){
         //single pixel rectangle
         //Rect sliv_rect = new Rect(0, 0, 1, DEFAULT_HEIGHT);
@@ -80,140 +76,23 @@ public class ScrollingBitmap {
             matrix.setTranslate(-1,0);
 
             paint.setColor(Color.CYAN);
-
-            //I don't think we need here
-            //if (mScrollingBitmap==null){
-            //   Log.d(TAG, "Creating new bitmap");
-            //   mScrollingBitmap = Bitmap.createBitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
-            //}        
-
-            //We set these up in the constructor
-            //Log.d(TAG, "Creating full buffered bitmap");
-            //mBufferBitmap = Bitmap.createBitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
-            //mBufferCanvas = new Canvas(mBufferBitmap);
-
-
-            //if(mSliverBitmap==null){
-            //    Log.d(TAG, "Creating slivered buffered bitmap");
-            //    mSliverBitmap = Bitmap.createBitmap(rect.width(), rect.height(), Bitmap.Config.ARGB_8888);
-            //    mNewSliverCanvas = new Canvas(mNewSliverBitmap);
-            //}
-
-            //Log.d(TAG, "Drawing points to canvas(bitmap): " + Arrays.toString(canvas_pts));
-
-            //Canvas c = new Canvas(mScaledBitmap);
-
-            //Draw this vertical sliver of vectored points
-            
-
             mSliverCanvas.drawPoints(canvas_pts, paint);
             
-            //TESTING
-            //float[] test_pts = new float[]{0,0,0,100,0,200,0,300,0,400};
-            //mSliverCanvas.drawPoints(test_pts, paint);
-            //TESTING
-            //Canvas dbgcc = new Canvas(mSliverBitmap);
-            //dbgcc.drawColor(Color.YELLOW);
-            
-
-
             //copy the sliver onto the previous bitmap in the (should be empty) sliver spot
             //draw the previous bitmap to the canvas
             mBufferCanvas.drawBitmap(mSliverBitmap, mSliverSrcRect, mSliverDstRect, paint);
 
-            //TESTING
-            //String tx1 = "B" + testing + mBufferBitmap.isMutable();
-            //if(testing<30){
-            //    paint.setColor(Color.RED);
-            //    mBufferCanvas.drawText(tx1,  100, 100, paint);
-            //}
-
-
-            //mScrollingCanvas.drawBitmap(mSliverBitmap, DEFAULT_WIDTH-50, 0, paint);
-
-            //mBufferCanvas.drawBitmap(mBufferBitmap, matrix, paint);
-
-            //Debug - works here
-            //Canvas dbgcc = new Canvas(mSliverBitmap);
-            //dbgcc.drawColor(Color.YELLOW);
-            //Canvas dbgc = new Canvas(mScrollingBitmap);
-            //dbgc.drawBitmap(mSliverBitmap,0,0,paint);
-
-            //mScrollingCanvas.drawBitmap(mScrollingBitmap, matrix, paint);
-
-
             //synchronized (mScrollingBitmap) {  
                 mScrollingCanvas.drawBitmap(mBufferBitmap, 0, 0, paint);
 
-                //TESTING
-                //String tx2 = "S" + testing + mScrollingBitmap.isMutable();
-                //if(testing<30){
-                //    paint.setColor(Color.CYAN);
-                //    mScrollingCanvas.drawText(tx2,  40, 40, paint);
-                //}
-                
-                
-                //TESTING
-                //String tx4 = "F" + testing;
-                //paint.setColor(Color.CYAN);
-                //mScrollingCanvas.drawText(tx2,  40+(float)testing, 40+(float)testing, paint);
-               
-              //callback is working
-                //Log.d(TAG, "Callback for drawing: " + testing);
-                
-                //TESTING
-                //testing++;
-                
-                //CALLBACK
-                //With ADDITIONAL PARAMTER FOR TESTING
                 this.mGraphDrawRegionCallback.onBitmapUpdate(mScrollingBitmap);
-
-                Log.d(TAG, "Retrund from Callback for drawing: ");
-
-
 
                 //Clear the existing sliver points
                 mSliverCanvas.drawColor(Color.DKGRAY);
 
-
-
-
                 //shift the full bitmap buffer down for the next sliver
                 mBufferCanvas.drawBitmap(mBufferBitmap, matrix, paint);
 
-                //TESTING
-                //String tx3 = "S2" + testing;
-                //if(testing<30){
-                //    paint.setColor(Color.CYAN);
-                //    mScrollingCanvas.drawText(tx3,  40, 60, paint);
-                //}
-                //c.drawColor(Color.DKGRAY);
-                //c.drawPoints(canvas_pts, paint);
-
-                //this.mGraphDrawRegionCallback.onBitmapUpdate(mBufferBitmap);
-
-
-
-
-            //}//Synchronized
-
-            /*
-            //Draw updated bitmap
-            Log.d(TAG, "Launching runner for graphing updated GRAPH Bitmap");
-
-            //TODO: Move DrawRegion HashMaps to a DrawRegion home
-            HThread graphThread = mPanelDrawer.mDrawRegionHThreads.get(DrawRegionNames.GRAPH);
-            if (graphThread.isAlive() && graphThread.handler!=null){
-                Log.d(TAG, "Attempting to draw scaled bitmap");
-                graphThread.handler.post( new Runnable(){
-                    @Override
-                    public void run() {
-                        mSelf.run(mPanelDrawer.mSurfaceHolder);
-
-                    };
-                });
-            }
-             */
         }
 
     }
@@ -222,11 +101,4 @@ public class ScrollingBitmap {
     public void setDrawRegion(DrawRegionType draw_reg){
         mGraphDrawRegionCallback = (DrawRegionGraph) draw_reg;
     }
-    
-    private void lazyLoadMe(){
-        
-    }
-    
-    
-
 }
