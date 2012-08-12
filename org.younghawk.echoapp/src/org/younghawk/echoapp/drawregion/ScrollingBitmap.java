c7bc0d703d5f3d6209eb8aa8f36b66439d4c8e83
Dave <dmarti21@yahoo.com>
2012-08-11 21:00:22 -0400

    changes ScrollBitmap paint and matrix to instance variables rather than local for performance reasons (though how much performance gain is unclear)

--- org.younghawk.echoapp/src/org/younghawk/echoapp/drawregion/ScrollingBitmap.java
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/drawregion/ScrollingBitmap.java
@@ -24,16 +24,21 @@ public class ScrollingBitmap {
     private Canvas mScrollingCanvas;
     private Bitmap mBufferBitmap;
     private Canvas mBufferCanvas;
-    
+
+    //Implemented as instance variables rather than local for performance reasons
+    private Paint mPaint = new Paint();
+    private Matrix mMatrix = new Matrix();
+
+    //Factory
     public static ScrollingBitmap create(){
-        //single pixel rectangle
-        //Rect sliv_rect = new Rect(0, 0, 1, DEFAULT_HEIGHT);
         Bitmap scroll_bmp = Bitmap.createBitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
         Bitmap buffer_bmp = Bitmap.createBitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
         Bitmap sliv_bmp   = Bitmap.createBitmap(1,DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
         return new ScrollingBitmap(sliv_bmp, scroll_bmp, buffer_bmp);
     }
     
+    
+    //Constructor
     private ScrollingBitmap(Bitmap sliv_bmp, Bitmap scroll_bmp, Bitmap buffer_bmp){
         this.mSliverBitmap = sliv_bmp;
         this.mSliverSrcRect = new Rect(0, 0, 1, sliv_bmp.getHeight());
@@ -47,16 +52,19 @@ public class ScrollingBitmap {
         this.mBufferCanvas = new Canvas(buffer_bmp);
         this.mScrollingBitmap = scroll_bmp;
         this.mScrollingCanvas = new Canvas(scroll_bmp);
-        
+        this.mMatrix.setTranslate(-1, 0);   //translate left by one
     }
+    
+
+    
+    
     
     //TODO: Implement Observer (use a callback interface)
     private DrawRegionGraph mGraphDrawRegionCallback;
     
     
     public void onVectorUpate(float[] vector_pts, float max, float min){
-        //Log.d(TAG, "Vector Update in Scrolling Bitmap called - callback is: " + mGraphDrawRegionCallback);
-        //testing++;
+
         //Only do work if there's a callback to receive it
         if(mGraphDrawRegionCallback!=null){
             //convert vector to canvas
@@ -71,19 +79,15 @@ public class ScrollingBitmap {
                 i+=2;
             }
 
-            Paint paint = new Paint();
-            Matrix matrix = new Matrix();
-            matrix.setTranslate(-1,0);
-
-            paint.setColor(Color.CYAN);
-            mSliverCanvas.drawPoints(canvas_pts, paint);
+            mPaint.setColor(Color.CYAN);
+            mSliverCanvas.drawPoints(canvas_pts, mPaint);
             
             //copy the sliver onto the previous bitmap in the (should be empty) sliver spot
             //draw the previous bitmap to the canvas
-            mBufferCanvas.drawBitmap(mSliverBitmap, mSliverSrcRect, mSliverDstRect, paint);
+            mBufferCanvas.drawBitmap(mSliverBitmap, mSliverSrcRect, mSliverDstRect, mPaint);
 
             //synchronized (mScrollingBitmap) {  
-                mScrollingCanvas.drawBitmap(mBufferBitmap, 0, 0, paint);
+                mScrollingCanvas.drawBitmap(mBufferBitmap, 0, 0, mPaint);
 
                 this.mGraphDrawRegionCallback.onBitmapUpdate(mScrollingBitmap);
 
@@ -91,7 +95,7 @@ public class ScrollingBitmap {
                 mSliverCanvas.drawColor(Color.DKGRAY);
 
                 //shift the full bitmap buffer down for the next sliver
-                mBufferCanvas.drawBitmap(mBufferBitmap, matrix, paint);
+                mBufferCanvas.drawBitmap(mBufferBitmap, mMatrix, mPaint);
 
         }
 
