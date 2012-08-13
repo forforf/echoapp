e7facd901b50c713c7cc84f16a3fde41ea0d9493
Dave <dmarti21@yahoo.com>
2012-08-12 22:12:03 -0400

    removed plotq code

--- org.younghawk.echoapp/src/org/younghawk/echoapp/PlotSupervisor.java
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/PlotSupervisor.java
@@ -46,10 +46,6 @@ public class PlotSupervisor implements Callback {
         public void run() {
             Log.d(TAG, "Plotter Q has " + mPlotter.mScaledSamples.size() + "elements now");
             Plotter.fillPlotQ();
-            ArrayDeque<Float> last_pts = Plotter.PlotQ[0];
-            if(last_pts!=null) {
-                Log.d(TAG, "Num of pts in last position of PlotQ: " + last_pts.size());    
-            }
             
             if (!pauseQCheck){
                 mPlotterHandler.postDelayed(checkingQ, (long) (Plotter.PX_DWELL_TIME * 1000) );
@@ -71,15 +67,7 @@ public class PlotSupervisor implements Callback {
             Handler plotHandler = null;
             if (plotLooper!=null) {
                 plotHandler = new Handler(plotLooper);
-               // plotHandler = new Handler(plotLooper){
-               //     public void handleMessage(Message msg) {
-               //         Log.d(TAG, "Finally received a msg: " + msg.what);
-               //         int[] audio_buffer = (int[]) msg.obj;
-               //         Log.d(TAG, "audio_buffer size: " + audio_buffer.length);
-               //         mPlotter.addToQ(audio_buffer);
-               //         //Log.d(TAG, "Q Size: " + mPlotter.mScaledSamples.size());
-               //     }
-               // };
+
             } else {
                 Log.e(TAG, "Plot Looper was null, was thread started?");
             }
@@ -110,101 +98,8 @@ public class PlotSupervisor implements Callback {
     //will require this method to execute via a thread handler post, and
     //flushing and stitching buffers together would need to be handled.
     public void onBufferData(Object objBuffer){
-        /*
-        short[] buffer = (short[]) objBuffer;
-        mBuffer = buffer;
-        Log.d(TAG, "PlotSupervisor (main thread) notified of buffer with " + buffer.length + " samples");
-        Log.d(TAG, "Accessing AudioDataRegion Bitmap");
-        
-        
-        //Thread to Update Bitmaps with audio graphin region with audio buffer data.
-        if (Panel.mAudioDataRegion!=null){
-            //map to region
-
-            mPlotterHandler.post(new Runnable(){
-                @Override
-                public void run() {
-                    Log.d(TAG, "Get audio region bitmap");
-                    Rect adr_rect = Panel.mAudioDataRegion.getDirtyRect();
-                    CollectionGrapher audioPlot = CollectionGrapher.create(adr_rect.left,adr_rect.top,adr_rect.width(),adr_rect.height(), mBuffer);
-                    Paint paint = new Paint();
-                    paint.setColor(Color.CYAN);
-                    //Here we get the bitmap, update it and commit it.
-                    synchronized(Panel.mAudioDataRegion) {
-                        Canvas c = new Canvas(Panel.mAudioDataRegion.getUnsafeBitmap());
-                        c.drawPoints(audioPlot.mCanvasPts, paint);
-                        Panel.mAudioDataRegion.updateDrawRegion();
-                    }
-                } //end run()
-            }); // end post
-        }
-                
-        //Thread loop to graph any updated regions - Keep together with executor
-        //TODO: Figure out if this belongs here or not.
-        Runnable checkAudioUpdates = new Runnable(){
-            @Override
-            public void run() {
-                boolean runThread = (!Thread.currentThread().isInterrupted() && !Panel.mStopRunningThreads);
-                while(runThread){
-                    if(!Panel.mAudioDataRegion.isConsumed){
-                        SurfaceHolder holder = mPanel.getHolder();
-                        Rect dirty_rect = Panel.mAudioDataRegion.getDirtyRect();
-                        Canvas c = holder.lockCanvas(dirty_rect);
-                        try{
-                            //This is the point in the thread that we call the onDraw method in our Panel class
-                            synchronized (holder) {
-                                //draw here
-                                c.drawBitmap(Panel.mAudioDataRegion.consumeBitmap(), dirty_rect.left, dirty_rect.top, null);
-                            }
-                        } finally {
-                            // do this in a finally so that if an exception is thrown
-                            // during the above, we don't leave the Surface in an
-                            // inconsistent state
-                            if (c != null) {
-                                holder.unlockCanvasAndPost(c);
-                            }
-                        }    
-                    }
-                    try {
-                        Thread.sleep(20);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        if(Thread.currentThread().isInterrupted()){
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }  
-            }
-            
-        };
-        
-        //TODO: Temporary workaround while refactoring
-        HandlerThreadExecutor temp = new HandlerThreadExecutor();
-        temp.execute(checkAudioUpdates);
-        //Panel.mSvHandler.post(checkAudioUpdates);
-        //Panel.mExecutor.execute(checkAudioUpdates);
-        //Keep the above with the runnable
-        
-        //int[] int_buffer = new int[buffer.length];
-        //for(int i=0;i<buffer.length;i++){
-        //    int_buffer[i] = (int) buffer[i];
-        //}
-        //Log.d(TAG, "PlotSupervisor (main thread) notified of buffer with " + buffer.length + " samples");
-        //mPlotter.pushAudioData(int_buffer);
-        //Log.d(TAG, "Attempting to send message to PlotRunnerDeadCode");
-        //PlotRunnerDeadCode plotRunner = PlotRunnerDeadCode.create(mPlotterHandler);
-        //mPlotterHandler.post(plotRunner);
-        //Message bufferMsg = Message.obtain(mPlotterHandler, MsgIds.BUFFER_DATA, int_buffer);
-        //Log.d(TAG, "Sending Message");
-        //mPlotterHandler.dispatchMessage(bufferMsg);
-        //plotRunner.handler.sendEmptyMessage(99);
-        //Log.d(TAG, "Message sent");
-       */ 
     }
     
-    //public float[] getPlotLineData(){
-    //    return mPlotter.getPlotData();
-    //}
     
     public void startQCheck() {
         Log.d(TAG, "Starting Q Check");
@@ -228,9 +123,6 @@ public class PlotSupervisor implements Callback {
         case MsgIds.BUFFER_DATA:
           onBufferData(msg.obj);
           break;
-        //case MsgIds.FILTER_DATA:
-        //    onFilterData(msg.obj);
-        //    break;
         }
         return false;
     }
--- org.younghawk.echoapp/src/org/younghawk/echoapp/Plotter.java
+++ org.younghawk.echoapp/src/org/younghawk/echoapp/Plotter.java
@@ -30,58 +30,17 @@ public class Plotter {
     //TODO: Figure out optimum instantiation size
     //TODO: Refactor so it isn't static?
     public static ArrayDeque<Float> mScaledSamples = new ArrayDeque<Float>();
-    public static final ArrayDeque<Float>[] PlotQ = new ArrayDeque[PLOT_WIDTH];
     public static boolean plotReady = false;
     private static ScrollingBitmap scr_bmp;
-    private static ArrayDeque<Float> mRecycledArrayDeque;
-    
-    //Recycle the ArrayDeque that falls off the queue to be used for
-    //the next item added to the queue
-    //Adds to the recycle
-    //private static void setRecycleDeque(ArrayDeque<Float> fell_off){
-    //    fell_off.clear();
-    //    mRecycledArrayDeque = fell_off;
-    //}
-    //private static ArrayDeque<Float> getRecycleDeque(){
-    //    if(mRecycledArrayDeque==null){
-    //        Log.e(TAG, "No Array Deque was found to be recycled!!!");
-    //        mRecycledArrayDeque = new ArrayDeque<Float>();
-    //    }
-    //    if(!mRecycledArrayDeque.isEmpty()){
-    //        mRecycledArrayDeque.clear();
-    //    }
-    //    return mRecycledArrayDeque;
-    //}
+
     
     //Refactor so this isn't static
     public static synchronized void fillPlotQ(){
-        //TESTING WITHOUT PlotQ
-        //shift PlotQ down 1
-        //for(int i=1;i<PLOT_WIDTH;i++) {
-            //ORIGINAL
-            //PlotQ[i-1] = PlotQ[i];
-            //TESTING
-            //noop
-        //}
-    
-        //Add new y values at end of PlotQ
-        //PlotQ[PLOT_WIDTH-1] = getRecycleDeque();
-        //if (PlotQ[PLOT_WIDTH-1]==null) {
-        //    //Log.d(TAG, "Last element was null");
-        //    PlotQ[PLOT_WIDTH-1] = new ArrayDeque();
-        //} else {
-        //    //Log.d(TAG, "Last element was not null");
-        //    PlotQ[PLOT_WIDTH-1].clear();
-        //}
              
         if (Plotter.mScaledSamples.size()>0) {
 
             float[] test_vector_pts = new float[PTS_PER_PX];
             for(int i=0;i<PTS_PER_PX;i++){
-                //test_vector_pts[i] = Plotter.mScaledSamples.peekFirst();
-                
-                //PlotQ[PLOT_WIDTH-1].add( Plotter.mScaledSamples.removeFirst() ); 
-                //PlotQ[PLOT_WIDTH-1].add( Plotter.mScaledSamples.peekFirst() );
                 test_vector_pts[i] = Plotter.mScaledSamples.removeFirst();
             }
                        
